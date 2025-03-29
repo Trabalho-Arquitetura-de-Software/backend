0367f4f4f733034d1e6cb449e48827c739b34361
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-03-29 20:42:03 -0300

    chore(group): manutenção do repositório JPA de grupo

--- management_service/src/main/java/ucsal/br/api/management_service/repository/GroupRepository.java
+++ management_service/src/main/java/ucsal/br/api/management_service/repository/GroupRepository.java
@@ -1,4 +1,9 @@
 package ucsal.br.api.management_service.repository;
 
-public interface GroupRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ucsal.br.api.management_service.entity.GroupEntity;
+
+import java.util.UUID;
+
+public interface GroupRepository extends JpaRepository<GroupEntity, UUID> {
 }
