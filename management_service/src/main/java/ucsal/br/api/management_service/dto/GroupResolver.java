9c9b43e96a5607b04c57de39661f5acf6102bf9a
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-03-29 20:43:19 -0300

    feat(group): resolver GraphQL com mutation saveGroup usando GroupInput

--- management_service/src/main/java/ucsal/br/api/management_service/dto/GroupResolver.java
+++ management_service/src/main/java/ucsal/br/api/management_service/dto/GroupResolver.java
@@ -1,4 +1,34 @@
-package ucsal.br.api.management_service.dto;
+package ucsal.br.api.management_service.resolver;
 
-public class GroupResolver {
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ucsal.br.api.management_service.dto.GroupDTO;
+import ucsal.br.api.management_service.dto.input.GroupInput;
+import ucsal.br.api.management_service.service.GroupService;
+import ucsal.br.api.management_service.service.UserService;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class GroupResolver implements GraphQLMutationResolver {
+
+    @Autowired
+    private GroupService groupService;
+
+    @Autowired
+    private UserService userService;
+
+    @Transactional
+    public GroupDTO saveGroup(GroupInput input) {
+        return groupService.save(
+                input.getName(),
+                input.isAvailableForProjects(),
+                input.getCoordinatorId(),
+                input.getStudentIds()
+        );
+    }
 }
