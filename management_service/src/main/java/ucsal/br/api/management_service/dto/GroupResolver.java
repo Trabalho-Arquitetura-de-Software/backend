04774a754702da3c2aa664c06587da32bbba2fb4
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-03-31 20:06:48 -0300

    refactor(resolver): move GroupResolver para pacote controller

--- management_service/src/main/java/ucsal/br/api/management_service/dto/GroupResolver.java
+++ management_service/src/main/java/ucsal/br/api/management_service/dto/GroupResolver.java
@@ -10,6 +10,7 @@ import ucsal.br.api.management_service.dto.input.GroupInput;
 import ucsal.br.api.management_service.service.GroupService;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class GroupResolver {
@@ -40,5 +41,37 @@ public class GroupResolver {
     @QueryMapping
     public List<GroupDTO> findAllGroups() {
         return groupService.getAllGroups();
+    }
+
+    /**
+     * Query para buscar um grupo por ID
+     */
+    @QueryMapping
+    public GroupDTO findGroupById(@Argument UUID id) {
+        return groupService.getGroupById(id);
+    }
+
+    /**
+     * Mutation para atualizar um grupo
+     */
+    @Transactional
+    @MutationMapping
+    public GroupDTO updateGroup(
+            @Argument UUID id,
+            @Argument String name,
+            @Argument Boolean availableForProjects,
+            @Argument UUID coordinatorId,
+            @Argument List<UUID> studentIds
+    ) {
+        return groupService.update(id, name, availableForProjects, coordinatorId, studentIds);
+    }
+
+    /**
+     * Mutation para deletar um grupo
+     */
+    @Transactional
+    @MutationMapping
+    public GroupDTO deleteGroup(@Argument UUID id) {
+        return groupService.delete(id);
     }
 }
