0ae2ca7a4f0251a8173c1de1c6f6941dc40cb5f1
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-03-31 20:06:56 -0300

    fix(repository): adiciona anotação @Repository ao GroupRepository

--- management_service/src/main/java/ucsal/br/api/management_service/repository/GroupRepository.java
+++ management_service/src/main/java/ucsal/br/api/management_service/repository/GroupRepository.java
@@ -1,9 +1,11 @@
 package ucsal.br.api.management_service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ucsal.br.api.management_service.entity.GroupEntity;
 
 import java.util.UUID;
 
+@Repository
 public interface GroupRepository extends JpaRepository<GroupEntity, UUID> {
 }
