20e07516e2fee6cd701be522a85b581bb97c30f2
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-04-02 23:55:41 -0300

    Adiciona construtor em UserDTO baseado em UserEntity

--- management_service/src/main/java/ucsal/br/api/management_service/dto/UserDTO.java
+++ management_service/src/main/java/ucsal/br/api/management_service/dto/UserDTO.java
@@ -1,41 +1,37 @@
 package ucsal.br.api.management_service.dto;
 
-import ucsal.br.api.management_service.entity.GroupEntity;
+import ucsal.br.api.management_service.entity.UserEntity;
+import ucsal.br.api.management_service.utils.type.UserRole;
 
-import java.util.List;
 import java.util.UUID;
 
-public class GroupDTO {
-
+public class UserDTO {
     private UUID id;
     private String name;
-    private Boolean availableForProjects;
-    private UUID coordinatorId;
-    private List<UUID> studentsIds;
+    private String email;
+    private String password;
+    private UserRole role;
 
-    public GroupDTO() {}
-
-    public GroupDTO(String name, Boolean availableForProjects, UUID coordinatorId, List<UUID> studentsIds) {
-        this.name = name;
-        this.availableForProjects = availableForProjects;
-        this.coordinatorId = coordinatorId;
-        this.studentsIds = studentsIds;
-    }
-
-    public GroupDTO(UUID id, String name, Boolean availableForProjects, UUID coordinatorId, List<UUID> studentsIds) {
+    public UserDTO() {}
+    public UserDTO(UUID id, String name, String email, String password, UserRole role) {
         this.id = id;
         this.name = name;
-        this.availableForProjects = availableForProjects;
-        this.coordinatorId = coordinatorId;
-        this.studentsIds = studentsIds;
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
-
-    public GroupDTO(GroupEntity entity) {
-        this.id = entity.getId();
-        this.name = entity.getName();
-        this.availableForProjects = entity.isAvailableForProjects();
-        this.coordinatorId = entity.getCoordinatorId();
-        this.studentsIds = entity.getStudentsIds();
+    public UserDTO(String name, String email, String password, UserRole role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public UserDTO(UserEntity userEntity) {
+        this.id = userEntity.getId();
+        this.name = userEntity.getName();
+        this.email = userEntity.getEmail();
+        this.password = userEntity.getPassword();
+        this.role = userEntity.getRole();
     }
 
     public UUID getId() {
@@ -54,27 +50,25 @@ public class GroupDTO {
         this.name = name;
     }
 
-    public Boolean getAvailableForProjects() {
-        return availableForProjects;
+    public String getEmail() {
+        return email;
     }
 
-    public void setAvailableForProjects(Boolean availableForProjects) {
-        this.availableForProjects = availableForProjects;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public UUID getCoordinatorId() {
-        return coordinatorId;
+    public String getPassword() {
+        return password;
     }
 
-    public void setCoordinatorId(UUID coordinatorId) {
-        this.coordinatorId = coordinatorId;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public List<UUID> getStudentsIds() {
-        return studentsIds;
+    public UserRole getRole() {
+        return role;
     }
 
-    public void setStudentsIds(List<UUID> studentsIds) {
-        this.studentsIds = studentsIds;
-    }
+    public void setRole(UserRole role) {this.role = role; }
 }
